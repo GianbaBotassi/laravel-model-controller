fa107ddd55259bef435cf7cbe16fdef38c15c1a3
GianbaBotassi <gianba89@gmail.com>
2023-07-12 16:45:20 +0200

    Fix php.ini, add basic layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,18 @@
 </head>
 
 <body>
-    <h1>hello Jello</h1>
-    {{-- <li>{{ $movies }}</li> --}}
+    <div class="container">
+        @foreach ($movies as $movie)
+            <div class="card">
+                <h4 class="text-center">{{ $movie->title }}</h4>
+                <h6 class="text-center">{{ $movie->title }}</h6>
+                <div>
+                    <span class="d-inline">Anno: {{ $movie->date }}</span>
+                    <span>Voto: {{ $movie->vote }}</span>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </body>
 
 </html>
